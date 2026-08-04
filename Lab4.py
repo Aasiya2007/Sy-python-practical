69d000986cb312239cfa5eec4e7918b150f81095
Aasiya2007 <aasiyadastagirmomin2007@gmail.com>
2026-08-04 13:25:30 +0530

    Add files via upload

---
+++ Lab4.py
@@ -0,0 +1,19 @@
+print("**********Traffic Signal Simulation System***********")
+
+signal= input("enter a signal color:").lower()
+
+if signal=="red":
+  print("signal is red")
+  print( "action : stop")
+
+elif signal=="yellow":
+  print("signal is yellow")
+  print( "action : get ready")
+
+elif signal=="green":
+  print("signal is green")
+  print( "action : go")
+    
+else:
+  print("invalid color : " )
+  print("Enter red , yellow or green: ")

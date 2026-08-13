574e780b9b12b3dc485b7cbea8a71d30faf65cdc
Aasiya2007 <aasiyadastagirmomin2007@gmail.com>
2026-08-13 13:09:05 +0530

    Add files via upload

---
+++ Exercise3.2.py
@@ -0,0 +1,9 @@
+score = float(input(" Enter graduation score (%) :"))
+backlog = int(input(" Enter Number of Academic backlogs : "))
+
+if score >= 70 and backlog == 0:
+    print("  Candidate is Eligible for placement.")
+
+else:
+    print("  Candidate is not Eligible for placement.")
+    

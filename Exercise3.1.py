24910b748b37e0f294a60806b2a0555e1a084e55
Aasiya2007 <aasiyadastagirmomin2007@gmail.com>
2026-08-13 13:08:51 +0530

    Add files via upload

---
+++ Exercise3.1.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age:"))
+income = float(input(" Enter your income"))
+
+if age < 25 and income <300000:
+    print(" Congratulation ! you are qualify for the scholarship scheme.")
+
+else:
+    print("sorry ! you are not qualify for the scholarship scheme.  ")

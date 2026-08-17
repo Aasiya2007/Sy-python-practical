6876e46f7814f178b35c3dc0144611603e186040
Aasiya2007 <aasiyadastagirmomin2007@gmail.com>
2026-08-17 10:42:29 +0530

    Update Exercise3.2.py

--- Exercise3.2.py
+++ Exercise3.2.py
@@ -1,7 +1,10 @@
+print("*********Plasment Eligibility System***********")
+
 score = float(input(" Enter graduation score (%) :"))
 backlog = int(input(" Enter Number of Academic backlogs : "))
+interview = input(" Did the candidate clear the interview: ").lower()
 
-if score >= 70 and backlog == 0:
+if score >= 70 and backlog == 0 and interview == "yes":
     print("  Candidate is Eligible for placement.")
 
 else:

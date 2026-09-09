976b1fab84c2f3042e256bf1a990ba4cc24fa35c
Aasiya2007 <aasiyadastagirmomin2007@gmail.com>
2026-09-09 16:18:13 +0530

    Add files via upload

---
+++ Exersice8.1.py
@@ -0,0 +1,11 @@
+print("============= Name Sanitizer ==============")
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+first_name = first_name.strip().title()
+last_name = last_name.strip().title()
+
+full_name = first_name + " " + last_name
+
+print("\nClean Name:", full_name)
